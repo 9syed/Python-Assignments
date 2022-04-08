af0a3468c339d8478915a07292ba33a5b6e63343
9syed <9syed.mar@gmail.com>
2022-04-08 16:36:05 +0530

    Added some more code.

--- OOP.py
+++ OOP.py
@@ -1,57 +1,94 @@
 import pygame
 import random
 from blob import Blob
+import numpy as np
 
-GREEN_BLOBS = 10
-BLUE_BLOBS = 4
+GREEN_BLOBS = 12
+BLUE_BLOBS = 10
+RED_BLOBS = 8
 
-WIDTH = 800
-HEIGHT = 700
-WHITE = (0, 0, 0)
+WIDTH = 1000 
+HEIGHT = 780 
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
-Green = (0, 255, 0)
 
-display_game = pygame.display.set_mode((WIDTH, HEIGHT)) 
-pygame.display.set_caption("A Game") 
+game_display = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("A Game")
 clock = pygame.time.Clock()
 
+class BlueBlob(Blob):
+
+    def __init__(self, x_boundary, y_boundary):
+        Blob.__init__(self, (0, 0, 255), x_boundary, y_boundary)
+
+    def __add__(self, other_blob):
+        if other_blob.color == (255, 0, 0):
+            self.size -= other_blob.size
+            other_blob.size -= self.size
+        elif other_blob.color == (0, 255, 0):
+            self.size += other_blob.size
+            other_blob.size = 0
+        elif other_blob.color == (0, 0, 255):
+            pass
+        else:
+            raise Exception('Tried to combine one or multiple blobs of invalid colors.')
+
+class RedBlob(Blob):
+
+    def __init__(self, x_boundary, y_boundary):
+        Blob.__init__(self, (255, 0, 0), x_boundary, y_boundary)
+
 class GreenBlob(Blob):
-       
-    def __init__(self, color, x_boundary, y_boundary):
-        super().__init__(color, x_boundary, y_boundary)
-        self.color = Green
 
-    def fast_motion(self):
-        self.x += random.randrange(-10, 10)
-        self.y += random.randrange(-10, 10)
+    def __init__(self, x_boundary, y_boundary):
+        Blob.__init__(self, (0, 255, 0), x_boundary, y_boundary)
 
+def touching(b1,b2):
+       return np.linalg.norm(np.array([b1.x, b1.y])-np.array([b2.x,b2.y])) < (b1.size + b2.size)
 
-def environment(blob_list): 
-    display_game.fill(WHITE) 
+def manage_collisions(blob_list):
+    blues, reds, greens = blob_list
+    for blue_id, blue_blob, in blues.copy().items():
+        for other_blobs in blues, reds, greens:
+            for other_blob_id, other_blob, in other_blobs.copy().items():
+                if blue_blob == other_blob:
+                    pass
+                else:
+                    if touching(blue_blob, other_blob):
+                        blue_blob + other_blob
+                        if other_blob.size <= 0:
+                            del other_blobs[other_blob_id]
+                        if blue_blob.size <= 0:
+                            del blues[blue_id]
 
-    for blob_dict in blob_list: 
-        for blob_id in blob_dict: 
+    return blues, reds, greens
+
+def environment(blob_list):
+    game_display.fill(BLACK)
+    blues, reds, greens = manage_collisions(blob_list)
+    for blob_dict in blob_list:
+        for blob_id in blob_dict:
             blob = blob_dict[blob_id]
-            pygame.draw.circle(display_game, blob.color, [blob.x, blob.y], blob.size) 
-            blob.fast_motion()
+            pygame.draw.circle(game_display, blob.color, [blob.x, blob.y], blob.size)
+            blob.motion()
             blob.check_bounds()
 
     pygame.display.update()
-
+    return blues, reds, greens
 
 def main():
-    green_blobs = dict(enumerate([GreenBlob(Green,WIDTH,HEIGHT) for I in range(GREEN_BLOBS)])) 
-    blue_blobs = dict(enumerate([GreenBlob(BLUE,WIDTH,HEIGHT) for I in range(BLUE_BLOBS)]))  
-    while True: 
-        for event in pygame.event.get(): 
-            if event.type == pygame.QUIT: 
+    green_blobs = dict(enumerate([GreenBlob(WIDTH,HEIGHT) for i in range(GREEN_BLOBS)]))
+    blue_blobs = dict(enumerate([BlueBlob(WIDTH,HEIGHT) for i in range(BLUE_BLOBS)]))
+    red_blobs = dict(enumerate([RedBlob(WIDTH,HEIGHT) for i in range(RED_BLOBS)]))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-
-        environment([green_blobs,blue_blobs])
+        blue_blobs,red_blobs,green_blobs = environment([blue_blobs,red_blobs,green_blobs])
         clock.tick(60)
-        #print(blue_blob.x, blue_blob.y)
 
 if __name__ == '__main__':
     main()
